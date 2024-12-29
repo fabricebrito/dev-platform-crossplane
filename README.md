11648352df32eb65d1b43eee2c7ba4efe766f099
Fabrice Brito <fabrice.brito@terradue.com>
2024-12-29 10:45:39 +0100

    adds functions and kubernetes provider

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dev-platform-crossplane
+
+This repository provides a skaffold configuration to deploy crossplane and the Helm and Kubernetes providers.
+
--- install-providers.sh
+++ install-providers.sh
@@ -1,6 +1,48 @@
 #!/bin/bash
 
-# helm
+# function to check if provider is healthy
+check_provider_health() {
+    local provider=$1
+    local timeout=$2
+    local interval=$3
+    local start_time=$(date +%s)
+    
+    echo "Waiting for provider-$provider to have status True where type is Healthy..."
+    
+    # Loop until the condition is met or timeout
+    while true; do
+        # Get the status of the "Healthy" condition
+        status=$(kubectl get provider.pkg.crossplane.io/provider-$provider -o jsonpath='{.status.conditions[?(@.type=="Healthy")].status}')
+        
+        # If the status is True, exit the loop
+        if [[ "$status" == "True" ]]; then
+            echo "Provider $provider is Healthy."
+            break
+        fi
+        
+        # Check if the timeout has been reached
+        current_time=$(date +%s)
+        elapsed_time=$((current_time - start_time))
+        
+        if (( elapsed_time >= timeout )); then
+            echo "Timeout reached. Provider $provider did not become Healthy in time."
+            exit 2
+        fi
+        
+        # Sleep for the interval before checking again
+        sleep $interval
+    done
+}
+
+# function to check if CRD is established
+check_crd_established() {
+    local crd=$1
+    local timeout=$2
+    
+    echo "Waiting for CRD $crd to be established..."
+    
+    kubectl wait --for=condition=established crd/$crd --timeout=${timeout}s
+}
 
 # Timeout duration
 TIMEOUT=180
@@ -9,39 +51,25 @@ INTERVAL=5
 # Start time
 START_TIME=$(date +%s)
 
+# helm provider
 echo "Applying Helm provider configuration..."
 kubectl apply -f provider/helm.yaml || exit 1
 
-echo "Waiting for provider-helm to have status True where type is Healthy..."
+# invoke function to check if provider is healthy
+check_provider_health helm $TIMEOUT $INTERVAL || exit 2
 
-# Loop until the condition is met or timeout
-while true; do
-    # Get the status of the "Healthy" condition
-    STATUS=$(kubectl get provider.pkg.crossplane.io/provider-helm -o jsonpath='{.status.conditions[?(@.type=="Healthy")].status}')
-    
-    # If the status is True, exit the loop
-    if [[ "$STATUS" == "True" ]]; then
-        echo "Provider Helm is Healthy."
-        break
-    fi
-    
-    # Check if the timeout has been reached
-    CURRENT_TIME=$(date +%s)
-    ELAPSED_TIME=$((CURRENT_TIME - START_TIME))
-    
-    if (( ELAPSED_TIME >= TIMEOUT )); then
-        echo "Timeout reached. Provider Helm did not become Healthy in time."
-        exit 2
-    fi
-    
-    # Sleep for the interval before checking again
-    sleep $INTERVAL
-done
-
-echo "Waiting for CRDs to be established..."
-kubectl wait --for=condition=established crd/providerconfigs.helm.crossplane.io --timeout=180s || exit 2
+check_crd_established providerconfigs.helm.crossplane.io $TIMEOUT || exit 2
 
 echo "Applying helm-provider-config.yaml..."
 kubectl apply -f provider-config/helm.yaml || exit 3
 
+# kubernetes provider
+echo "Applying Kubernetes provider configuration..."
+kubectl apply -f provider/kubernetes.yaml || exit 4
 
+check_provider_health kubernetes $TIMEOUT $INTERVAL
+
+check_crd_established providerconfigs.kubernetes.crossplane.io $TIMEOUT || exit 5
+
+echo "Applying kubernetes-provider-config.yaml..."
+kubectl apply -f provider-config/kubernetes.yaml || exit 6
